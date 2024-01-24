42b98f63fd8e3ea1d20381d786d49b6325052aee
jfelipe14 <juan_fquintero@soy.sena.edu.co>
2024-01-24 10:15:57 -0500

    tablas de verdad en Java

---
+++ java/tablasdeverdad/TablasDeVerdad.java
@@ -0,0 +1,18 @@
+public class TablasDeVerdad {
+    public static void main(String[] args) {
+        boolean v = true;
+        boolean f = false;
+
+        System.out.println("Tabla de verdad del operador lógico AND");
+        System.out.println("v && v = " + (v && v));
+        System.out.println("v && f = " + (v && f));
+        System.out.println("f && v = " + (f && v));
+        System.out.println("f && f = " + (f && f));
+
+        System.out.println("Tabla de verdad del operador lógico OR");
+        System.out.println("v || v = " + (v || v));
+        System.out.println("v || f = " + (v || f));
+        System.out.println("f || v = " + (f || v));
+        System.out.println("f || f = " + (f || f));
+    }
+}
