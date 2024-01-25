805ad77fa618a7e624ef3637efcb17b38b337661
jfelipe14 <juan_fquintero@soy.sena.edu.co>
2024-01-24 19:15:12 -0500

    reorganizacion de discos

--- java/discos/Discos.java
+++ java/discos/Discos.java
@@ -3,20 +3,10 @@
 public class Discos {
     public static void main(String[] args) {
         // declaracion de variables
-        int capacidadDisco, capacidadCD, cantidadCDs;
-
-        // agregar un valor a una variable
-        capacidadDisco = 100; // gigabyte
-        capacidadCD = 700; // megabyte
-
+        int cantidadCDs;
         // operaciones
-        cantidadCDs = (capacidadDisco * 1024) / capacidadCD;
-
-        cantidadCDs += 1;
-
+        cantidadCDs = (500 * 1024) / 700 + 1;
         // mostrar datos
-        System.out.println("La cantidad de CDs necesarios para hacer una copia de seguridad de un disco de "
-                + capacidadDisco + " GB es: " + cantidadCDs);
-
+        System.out.println(cantidadCDs);
     }
 }
